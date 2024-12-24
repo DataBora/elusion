d3accd14cf6c7489c38186a36355b58918073e79
Borivoj Grujicic <borivoj.grujicic@gmail.com>
2024-12-24 19:04:54 +0100

    raw sql support

--- src/main.rs
+++ src/main.rs
@@ -38,35 +38,13 @@ async fn main() -> ElusionResult<()> {
         ("HomeOwner","CHAR", true)
     ];
 
-    // let products_columns = vec![
-    //     ("ProductKey", "INT", false),
-    //     ("ProductSubcategoryKey", "INT", false),
-    //     ("ProductSKU", "VARCHAR", false),
-    //     ("ProductName", "VARCHAR", false),
-    //     ("ModelName", "VARCHAR", false),
-    //     ("ProductDescription", "VARCHAR", false),
-    //     ("ProductColor", "VARCHAR", false),
-    //     ("ProductSize", "VARCHAR", false),
-    //     ("ProductStyle", "VARCHAR", false),
-    //     ("ProductCost", "DOUBLE", false),
-    //     ("ProductPrice", "DOUBLE", false)
-    // ];
 
-    // let subcategory_columns = vec![
-    //     ("ProductSubcategoryKey", "INT", false),
-    //     ("SubcategoryName", "VARCHAR", false),
-    //     ("ProducCateforyKey", "INT", false)
-    // ];
 
-    let sales_data = "C:\\Borivoj\\RUST\\Elusion\\SalesData2022.csv";
-    let customers_data = "C:\\Borivoj\\RUST\\Elusion\\Customers.csv";
-    // let products = "C:\\Borivoj\\RUST\\Elusion\\Products.csv";
-    // let subcategory = "C:\\Borivoj\\RUST\\Elusion\\ProductSubcategory.csv";
+    let sales_data = "C:\\Users\\BorivojGrujičić\\RUST\\divcibare\\SalesData2022.csv";
+    let customers_data = "C:\\Users\\BorivojGrujičić\\RUST\\divcibare\\Customers.csv";
 
     let df_sales = CustomDataFrame::new(sales_data, sales_columns, "sales").await; 
     let df_customers = CustomDataFrame::new(customers_data, customers_columns, "customers").await; 
-    // let df_products = CustomDataFrame::new(products, products_columns, "products").await; 
-    // let df_subcategory = CustomDataFrame::new(subcategory, subcategory_columns, "subcategory").await; 
 
 
     let join_df = df_sales.clone()
@@ -87,55 +65,6 @@ async fn main() -> ElusionResult<()> {
     join_df.display().await?;
     // join_df.display_query_plan();
 
-// ============ CTE
-  //  CTE for aggregated sales data
- 
-        // let aggregated_sales = df_sales.clone()
-        //     .aggregation(vec![
-        //         AggregationBuilder::new("OrderQuantity").sum().alias("total_order_quantity"),
-        //         AggregationBuilder::new("OrderLineItem").count().alias("total_orders"),
-        //     ])
-        //     .group_by(vec!["sales.CustomerKey"])
-        //     .select(vec!["CustomerKey", "total_order_quantity", "total_orders"]);
-            
-        // // println!("Aggregated Sales Schema: {:?}", aggregated_sales.df.schema());
-
-        // // for cte in &aggregated_sales.ctes {
-        // // println!("CTE Registered: {:?}", cte.schema.fields());
-        // // }
-
-        // let cte_query = aggregated_sales.df_alias("agg_sales")
-        // .join(
-        //     df_customers,
-        //     "agg_sales.CustomerKey == customers.CustomerKey",
-        //     "INNER",
-        // )
-        // .select(vec![
-        //     "total_order_quantity", 
-        //     "cust.FirstName",
-        //     "cust.LastName",
-        //     "cust.EmailAddress",
-        // ]);
-
-        // cte_query.limit(10).display().await?;
-
-
-// ========== WINDOW Function
-
-    // let window_query = df_sales
-    //     .select(vec!["CustomerKey", "OrderDate", "OrderQuantity"])
-    //     .window(
-    //         "RANK",                  // Window function: RANK
-    //         "OrderQuantity",         // Column to calculate rank
-    //         vec!["CustomerKey"],     // PARTITION BY CustomerKey
-    //         vec!["OrderDate"],       // ORDER BY OrderDate
-    //         Some("OrderRank"),       // Alias for the window function
-    //     )
-    //     .order_by(vec!["CustomerKey", "OrderRank"], vec![true, true]) // Order by CustomerKey and rank
-    //     .limit(10);
-
-    // window_query.display_query();
-    // window_query.display().await?;
 
 //==================================
 
@@ -154,9 +83,10 @@ async fn main() -> ElusionResult<()> {
         ("billable_value", "NUMBER", false),
     ];
 
-    let sales_path = "C:\\Borivoj\\RUST\\Elusion\\sales_order_report.csv";
-
+    let sales_path = "C:\\Users\\BorivojGrujičić\\RUST\\divcibare\\sales_order_report.csv";
     let sales_order_data = CustomDataFrame::new(sales_path, sales_order_columns, "sales_orders").await;
+
+    // ========= FILTERING
 
     let result_sales = sales_order_data.clone()
             .select(vec!["customer_name", "order_date", "billable_value"])
@@ -167,37 +97,100 @@ async fn main() -> ElusionResult<()> {
     result_sales.display_query();   
     result_sales.display().await?;
 
-// // //=============================================
-let result_df = sales_order_data
-    .aggregation(vec![
-        AggregationBuilder::new("billable_value").sum().alias("total_sales"),
-        AggregationBuilder::new("billable_value").avg().alias("avg_sales")
-    ])
-    .group_by(vec!["customer_name", "order_date"])
-    .having("total_sales > 1000")
-    .select(vec!["customer_name", "order_date", "total_sales", "avg_sales"]) 
-    .order_by(vec!["total_sales"], vec![false])
-    .limit(10);
+    // AGGREGATION =============================================
+    let result_df = sales_order_data
+        .aggregation(vec![
+            AggregationBuilder::new("billable_value").sum().alias("total_sales"),
+            AggregationBuilder::new("billable_value").avg().alias("avg_sales")
+        ])
+        .group_by(vec!["customer_name", "order_date"])
+        .having("total_sales > 1000")
+        .select(vec!["customer_name", "order_date", "total_sales", "avg_sales"]) 
+        .order_by(vec!["total_sales"], vec![false])
+        .limit(10);
 
 
     result_df.display_query();
     result_df.display().await?;
 
-    let sales_columns = df_sales
-        .select(vec!["CustomerKey as customer_key"])
+    //=================== ALIAS IN SELECT ==================
+    let sales_columns = df_sales.clone()
+        .select(vec!["CustomerKey as customer_key","StockDate AS date"])
         .limit(10);
 
     sales_columns.display().await?;
-    
 
-//     //======= writing parquet
+    // ============ raw sql
 
-    
+    let raw_sql = "
+        WITH agg_sales AS (
+            SELECT
+                CustomerKey,
+                SUM(OrderQuantity) AS total_order_quantity,
+                COUNT(OrderLineItem) AS total_orders
+            FROM sales
+            GROUP BY CustomerKey
+        ),
+        customer_details AS (
+            SELECT
+                *
+            FROM customers
+        )
+        SELECT
+            cd.*,
+            asales.total_order_quantity,
+            asales.total_orders
+        FROM agg_sales asales
+        INNER JOIN customer_details cd ON asales.CustomerKey = cd.CustomerKey
+        ORDER BY asales.total_order_quantity DESC
+        LIMIT 100;
+    ";
 
-//     result_df
-//         .write_to_parquet("overwrite", "C:\\Borivoj\\RUST\\Elusion\\test.parquet", None)
-//         .await
-//         .expect("Failed to write to Parquet");
+    let raw_cte = df_sales.execute_sql(raw_sql, "top_customers", &[&df_customers]).await?;
+    raw_cte.display().await?;
+
+    // =================== JSON
+
+    // let json_schema = vec![
+    //     "","",""
+    // ];
+    // let json_file_path = "C:\\Users\\BorivojGrujičić\\RUST\\divcibare\\test.json";
+
+    // let df_json = CustomDataFrame::load(json_file_path, json_schema, "my_json").await;
+
+    // df_json.display().await?;
+
+    // =============== CTE
+
+    // let aggregated_sales = df_sales.clone()
+    //         .aggregation(vec![
+    //             AggregationBuilder::new("OrderQuantity").sum().alias("total_order_quantity"),
+    //             AggregationBuilder::new("OrderLineItem").count().alias("total_orders"),
+    //         ])
+    //         .group_by(vec!["sales.CustomerKey"])
+    //         .with_cte( "agg_sales");
+
+    // let joined_df = agg_sales
+    //         .join(
+    //             df_customers,
+    //             "agg_sales.CustomerKey == cust.CustomerKey",
+    //             "INNER",
+    //         )
+    //         .select(vec![
+    //             "agg_sales.total_order_quantity", 
+    //             "cust.FirstName",
+    //             "cust.LastName",
+    //             "cust.EmailAddress",
+    //         ]);
+
+    //     joined_df.display().await?;
+
+    //======= writing parquet
+
+    raw_cte
+        .write_to_parquet("overwrite", "C:\\Users\\BorivojGrujičić\\RUST\\divcibare\\raw_cte.parquet", None)
+        .await
+        .expect("Failed to write to Parquet");
 
 //     result_df
 //         .write_to_parquet(
